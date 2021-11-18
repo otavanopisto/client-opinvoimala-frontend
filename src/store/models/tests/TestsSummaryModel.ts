97eb354379b260043bffa41855046ca0694894a9
Toni Weckroth <toni.weckroth@geniem.com>
2021-11-18 10:53:17 +0200

    OPI-53: Initial view for well-being profile (#29)
    * OPI-53: Initial view for well-being profile
    
    * OPI-53: Fetch tests summary data from API

---
+++ src/store/models/tests/TestsSummaryModel.ts
@@ -0,0 +1,14 @@
+import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
+import { TestsSummaryCategoryModel } from '.';
+
+export const TestsSummaryModel = types.model({
+  stars: types.maybeNull(types.number),
+  summaryText: types.maybeNull(types.string),
+  detailsText: types.maybeNull(types.string),
+  categories: types.maybeNull(types.array(TestsSummaryCategoryModel)),
+});
+
+export interface ITestsSummaryModel
+  extends Instance<typeof TestsSummaryModel> {}
+export interface TestsSummary extends SnapshotOut<typeof TestsSummaryModel> {}
+export interface TestsSummaryIn extends SnapshotIn<typeof TestsSummaryModel> {}
---
+++ src/views/WellBeingProfile.tsx
@@ -0,0 +1,29 @@
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import Layout from '../components/Layout';
+import { useStore } from '../store/storeContext';
+
+export const WellBeingProfile: React.FC = observer(() => {
+  const { t } = useTranslation();
+
+  const {
+    tests: { testsSummaryState, testsSummary, fetchTestsSummary },
+  } = useStore();
+
+  const isBusy = testsSummaryState === 'FETCHING';
+
+  useEffect(() => {
+    fetchTestsSummary();
+  }, [fetchTestsSummary]);
+
+  const hero = {
+    title: t('view.well_being_profile.title'),
+  };
+
+  return (
+    <Layout hero={hero} isLoading={isBusy}>
+      TODO
+    </Layout>
+  );
+});
