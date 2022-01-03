c5e65b56e695515e8af1a32224fd0d3ada99930a
WeceW <toni.weckroth@gmail.com>
2022-01-03 14:38:09 +0200

    OPI-59: Cookiebot setup

--- src/views/CookieDeclaration.tsx
+++ src/views/CookieDeclaration.tsx
@@ -4,9 +4,7 @@ import { observer } from 'mobx-react-lite';
 import Layout from '../components/Layout';
 import { useStore } from '../store/storeContext';
 
-interface Props {}
-
-const CookieDeclaration: React.FC<Props> = observer(() => {
+const CookieDeclaration: React.FC = observer(() => {
   const { t } = useTranslation();
   const cookieDeclarationRef = useRef<HTMLDivElement>(null);
 
@@ -17,14 +15,16 @@ const CookieDeclaration: React.FC<Props> = observer(() => {
   const { cookiebotDomainGroupId } = settings?.scripts ?? {};
 
   useEffect(() => {
-    const scriptTag: any = document.createElement('script');
-    scriptTag.async = true;
-    scriptTag.type = 'text/javascript';
-    scriptTag.id = 'CookieDeclaration';
-    scriptTag.src = `https://consent.cookiebot.com/${cookiebotDomainGroupId}/cd.js`;
-    scriptTag['data-culture'] = 'FI';
+    if (cookiebotDomainGroupId) {
+      const scriptTag = document.createElement('script');
+      scriptTag.async = true;
+      scriptTag.type = 'text/javascript';
+      scriptTag.id = 'CookieDeclaration';
+      scriptTag.src = `https://consent.cookiebot.com/${cookiebotDomainGroupId}/cd.js`;
+      scriptTag.setAttribute('data-culture', 'FI');
 
-    cookieDeclarationRef.current?.appendChild(scriptTag);
+      cookieDeclarationRef.current?.appendChild(scriptTag);
+    }
   }, [cookiebotDomainGroupId]);
 
   const hero = {
