2a4866a0800f33554ffc630fc92103f7a0ce05f2
Joonas Lehtinen <joonas.lehtinen@geniem.com>
2022-04-27 13:50:30 +0300

    OPI-88_goals_component: WIP: component styles

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -1,58 +1,24 @@
-import React from 'react';
-import { Card, Divider, Grid } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { Divider, Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
+import { useStore } from '../store/storeContext';
 
-const mockdata = {
-  id: 'id',
-  title: 'Tavoitteet',
-  info_text: 'Aseta itsellesi enintään neljä tavoitetta.',
-  image: {
-    id: 20,
-    url: 'https://storage.googleapis.com/client-opinvoimala-dev/goal_throphy_556de02402/goal_throphy_556de02402.png',
-    alternativeText: '',
-    caption: '',
-  },
-  goals: [
-    {
-      id: 19,
-      description: 'Syö enemmän, mutta vähemmän kuin 160 merkkiä',
-      done: false,
-      created_at: '2022-04-26T11:49:57.000Z',
-      updated_at: '2022-04-26T11:49:57.000Z',
-    },
-    {
-      id: 18,
-      description:
-        'Katsele telkkarista enemmän jooga-ohjelmia, mutta ei täydellä mahalla tai saunan jälkeen edes vitsinä, koska me kaikki tiedetään miten siinä lopulta sitten käy',
-      done: false,
-      created_at: '2022-04-26T11:49:45.000Z',
-      updated_at: '2022-04-26T11:49:45.000Z',
-    },
-    {
-      id: 17,
-      description: 'Nuku pidempiä yöunia',
-      done: false,
-      created_at: '2022-04-26T11:49:38.000Z',
-      updated_at: '2022-04-26T11:49:38.000Z',
-    },
-    {
-      id: 16,
-      description: 'MUOKATTU',
-      done: true,
-      created_at: '2022-04-26T11:35:55.000Z',
-      updated_at: '2022-04-26T11:42:36.000Z',
-    },
-  ],
-};
+const Goal = styled.div<{ done?: boolean }>`
+  background-color: ${p => (p.done ? p.theme.color.primaryLightest : 'none')};
 
-const Goal = styled.li<{ done?: boolean }>`
-  background-color: ${p => (p.done ? 'green' : 'none')};
+  border-radius: 4px;
   ${p => p.theme.shadows[0]};
   ${p => p.theme.font.h4};
   line-height: 28px;
-  padding-right: 48px;
   margin-top: ${p => p.theme.spacing.lg};
   margin-bottom: ${p => p.theme.spacing.md};
+  padding: 32px;
+`;
+
+const GoalInfoText = styled.div`
+  font-family: 'Urbanist', sans-serif;
+  font-size: 24px;
 `;
 
 const StyledGrid = styled(Grid)`
@@ -64,25 +30,40 @@ const StyledGrid = styled(Grid)`
   }
 `;
 
-export const Goals: React.FC = () => {
+export const Goals: React.FC = observer(() => {
+  const {
+    goals: {
+      goals,
+      goalsInfo,
+      fetchGoals,
+      // addGoal,
+      // editGoal,
+      // markGoalDone,
+      // deleteGoal,
+    },
+  } = useStore();
+
+  useEffect(() => {
+    fetchGoals();
+  }, [fetchGoals]);
   return (
     <>
       <StyledGrid columns={2}>
-        <h2 id={mockdata.id}>{mockdata.title}</h2>
+        <h2>{goalsInfo?.title}</h2>
+        <img src={goalsInfo?.image.url} alt="new" />
+      </StyledGrid>
 
-        <img src={mockdata.image.url} alt="new" />
-      </StyledGrid>
-      {mockdata.info_text}
+      <GoalInfoText>{goalsInfo?.infoText}</GoalInfoText>
+
       <Divider section hidden aria-hidden="true" />
-      <StyledGrid columns={1}>
-        {mockdata.goals.map(({ id, description, done }) => (
-          <Grid.Column key={id}>
-            <Goal key={id} done={done}>
-              {description}
-            </Goal>
-          </Grid.Column>
-        ))}
-      </StyledGrid>
+
+      {goals.map(({ id, description, done }) => (
+        <ul>
+          <Goal key={id} done={done}>
+            <h4>{description}</h4>
+          </Goal>
+        </ul>
+      ))}
     </>
   );
-};
+});
