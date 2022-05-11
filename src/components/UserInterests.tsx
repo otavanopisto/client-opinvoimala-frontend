2775510b8fc186f665939205c0e7a437f72f9a64
Joonas Lehtinen <joonas.lehtinen@geniem.com>
2022-05-11 15:48:18 +0300

    OPI-94_user_interests: WIP styles

--- src/components/UserInterests.tsx
+++ src/components/UserInterests.tsx
@@ -1,12 +1,28 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect } from 'react';
+import { Divider, Icon as SemanticIcon } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 import { useStore } from '../store/storeContext';
 import { Button } from './inputs';
+import Card from './Card';
+
+const StyledGrid = styled(Grid)`
+  @media ${p => p.theme.breakpoint.mobile} {
+    &.ui.grid > .column:not(.row) {
+      padding-left: 0 !important;
+      padding-right: 0 !important;
+    }
+  }
+`;
 
 export const UserInterests: React.FC = observer(() => {
   const {
-    userInterests: { fetchUserInterests, state, userInterests, setUserTags },
+    userInterests: { fetchUserInterests, state, userInterests },
   } = useStore();
+
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (!['FETCHED', 'FETCHING', 'ERROR'].includes(state)) {
@@ -14,21 +30,32 @@ export const UserInterests: React.FC = observer(() => {
     }
   }, [fetchUserInterests, state]);
 
-  const postTags = () => {
-    setUserTags({ tags: [2] });
-  };
+  const handleSetTags = () => {};
 
   return (
-    <>
-      Sinua voi kiinnostaa
-      <Button id={'testi'} onClick={postTags}>
-        testaa tageja
-      </Button>
-      <ul>
+    <section>
+      <h2>{t('view.user_interests.title')}</h2>
+
+      <StyledGrid columns={3} stackable doubling stretched>
         {userInterests.map(interest => (
-          <li>{interest.title}</li>
+          <Grid.Column key={interest.id}>
+            <Card
+              title={interest.title}
+              text={interest.description}
+              tags={interest.tags}
+            />
+          </Grid.Column>
         ))}
-      </ul>
-    </>
+      </StyledGrid>
+
+      <Divider hidden aria-hidden="true" />
+      <Button
+        id="user-interests__set-tags-button"
+        text={t('view.user_interests.set_tags')}
+        color="primary"
+        icon={<SemanticIcon name="plus square outline" size="large" />}
+        onClick={handleSetTags}
+      />
+    </section>
   );
 });
